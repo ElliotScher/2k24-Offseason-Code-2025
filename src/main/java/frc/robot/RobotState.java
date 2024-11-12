4aebb25d537c2070f1fcf0d4e0e6fbd1b8a15a5e
ElliotScher <ecscher84@gmail.com>
2024-11-11 21:12:26 -0500

    first commit

---
+++ src/main/java/frc/robot/RobotState.java
@@ -0,0 +1,196 @@
+package frc.robot;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.subsystems.drive.DriveConstants;
+import frc.robot.subsystems.vision.Camera;
+import frc.robot.util.AllianceFlipUtil;
+import frc.robot.util.GeometryUtil;
+import lombok.Getter;
+import lombok.Setter;
+import org.littletonrobotics.junction.Logger;
+
+public class RobotState {
+  private static final InterpolatingDoubleTreeMap speakerShotAngleMap =
+      new InterpolatingDoubleTreeMap();
+  private static final InterpolatingDoubleTreeMap timeOfFlightMap =
+      new InterpolatingDoubleTreeMap();
+
+  @Getter
+  private static ControlData controlData =
+      new ControlData(new Rotation2d(), 0.0, new Rotation2d(), false, false, false);
+
+  @Getter @Setter private static double speakerFlywheelCompensation = 0.0;
+  @Getter @Setter private static double speakerAngleCompensation = 0.0;
+
+  private static final SwerveDrivePoseEstimator poseEstimator;
+  private static final SwerveDriveOdometry odometry;
+
+  private static Rotation2d robotHeading;
+  private static Rotation2d headingOffset;
+  private static SwerveModulePosition[] modulePositions;
+
+  static {
+    // Units: radians
+    speakerShotAngleMap.put(1.0051382994805276, Units.degreesToRadians(57.0));
+    speakerShotAngleMap.put(1.4924089439984491, 0.84);
+    speakerShotAngleMap.put(2.0188748058905883, 0.74);
+    speakerShotAngleMap.put(2.494223768158363, 0.66);
+    speakerShotAngleMap.put(2.997906851949344, 0.57);
+    speakerShotAngleMap.put(3.481117210151285, 0.5);
+    speakerShotAngleMap.put(3.992798130214426, 0.46);
+    speakerShotAngleMap.put(4.590536757726377, 0.44);
+    speakerShotAngleMap.put(4.9909464332643125, 0.42);
+    speakerShotAngleMap.put(5.508818126964896, 0.4);
+    speakerShotAngleMap.put(6.067253283488031, 0.37);
+
+    // Units: seconds
+    timeOfFlightMap.put(0.0, 0.0);
+
+    modulePositions = new SwerveModulePosition[4];
+
+    for (int i = 0; i < modulePositions.length; i++) {
+      modulePositions[i] = new SwerveModulePosition();
+    }
+
+    poseEstimator =
+        new SwerveDrivePoseEstimator(
+            DriveConstants.KINEMATICS, new Rotation2d(), modulePositions, new Pose2d());
+    odometry =
+        new SwerveDriveOdometry(DriveConstants.KINEMATICS, new Rotation2d(), modulePositions);
+    headingOffset = new Rotation2d();
+  }
+
+  public RobotState() {}
+
+  public static void periodic(
+      Rotation2d robotHeading,
+      long latestRobotHeadingTimestamp,
+      double robotYawVelocity,
+      Translation2d robotFieldRelativeVelocity,
+      SwerveModulePosition[] modulePositions,
+      Camera[] cameras,
+      boolean hasNoteLocked,
+      boolean hasNoteStaged,
+      boolean isIntaking) {
+
+    RobotState.robotHeading = robotHeading;
+    RobotState.modulePositions = modulePositions;
+
+    odometry.update(robotHeading, modulePositions);
+    poseEstimator.updateWithTime(Timer.getFPGATimestamp(), robotHeading, modulePositions);
+
+    for (Camera camera : cameras) {
+      double[] limelightHeadingData = {
+        robotHeading.minus(headingOffset).getDegrees(), 0.0, 0.0, 0.0, 0.0, 0.0
+      };
+      camera.getRobotHeadingPublisher().set(limelightHeadingData, latestRobotHeadingTimestamp);
+    }
+    NetworkTableInstance.getDefault().flush();
+    for (Camera camera : cameras) {
+
+      if (camera.getTargetAquired()
+          && !GeometryUtil.isZero(camera.getPrimaryPose())
+          && !GeometryUtil.isZero(camera.getSecondaryPose())
+          && Math.abs(robotYawVelocity) <= Units.degreesToRadians(15.0)
+          && Math.abs(robotFieldRelativeVelocity.getNorm()) <= 1.0) {
+        double xyStddevPrimary =
+            camera.getPrimaryXYStandardDeviationCoefficient()
+                * Math.pow(camera.getAverageDistance(), 2.0)
+                / camera.getTotalTargets()
+                * camera.getHorizontalFOV();
+        poseEstimator.addVisionMeasurement(
+            camera.getPrimaryPose(),
+            camera.getFrameTimestamp(),
+            VecBuilder.fill(xyStddevPrimary, xyStddevPrimary, Double.POSITIVE_INFINITY));
+        if (camera.getAverageDistance() <= 1.0) {
+          double xyStddevSecondary =
+              camera.getSecondaryXYStandardDeviationCoefficient()
+                  * Math.pow(camera.getAverageDistance(), 2.0)
+                  / camera.getTotalTargets()
+                  * camera.getHorizontalFOV();
+          poseEstimator.addVisionMeasurement(
+              camera.getSecondaryPose(),
+              camera.getFrameTimestamp(),
+              VecBuilder.fill(xyStddevSecondary, xyStddevSecondary, Double.POSITIVE_INFINITY));
+        }
+      }
+    }
+
+    // Speaker Shot Calculations
+    Translation2d speakerPose =
+        AllianceFlipUtil.apply(FieldConstants.Speaker.centerSpeakerOpening.toTranslation2d());
+    double distanceToSpeaker =
+        poseEstimator.getEstimatedPosition().getTranslation().getDistance(speakerPose);
+    Translation2d effectiveSpeakerAimingTranslation =
+        poseEstimator
+            .getEstimatedPosition()
+            .getTranslation()
+            .plus(robotFieldRelativeVelocity.times(timeOfFlightMap.get(distanceToSpeaker)));
+    double effectiveDistanceToSpeaker = effectiveSpeakerAimingTranslation.getDistance(speakerPose);
+    Rotation2d speakerRobotAngle =
+        speakerPose
+            .minus(effectiveSpeakerAimingTranslation)
+            .getAngle()
+            .minus(Rotation2d.fromDegrees(180.0 + 3.5));
+    double speakerTangentialVelocity =
+        -robotFieldRelativeVelocity.rotateBy(speakerRobotAngle.unaryMinus()).getY();
+    double speakerRadialVelocity = speakerTangentialVelocity / effectiveDistanceToSpeaker;
+
+    controlData =
+        new ControlData(
+            speakerRobotAngle,
+            speakerRadialVelocity,
+            new Rotation2d(speakerShotAngleMap.get(effectiveDistanceToSpeaker)),
+            hasNoteLocked,
+            hasNoteStaged,
+            isIntaking);
+
+    Logger.recordOutput(
+        "RobotState/Pose Data/Estimated Pose", poseEstimator.getEstimatedPosition());
+    Logger.recordOutput("RobotState/Pose Data/Odometry Pose", odometry.getPoseMeters());
+    Logger.recordOutput("RobotState/Pose Data/Heading Offset", headingOffset);
+    Logger.recordOutput(
+        "RobotState/Pose Data/Effective Speaker Aiming Pose",
+        new Pose2d(effectiveSpeakerAimingTranslation, speakerRobotAngle));
+    Logger.recordOutput(
+        "RobotState/Pose Data/Effective Distance To Speaker", effectiveDistanceToSpeaker);
+
+    Logger.recordOutput(
+        "RobotState/Control Data/Speaker Robot Angle", controlData.speakerRobotAngle());
+    Logger.recordOutput("RobotState/Control Data/Speaker Arm Angle", controlData.speakerArmAngle());
+    Logger.recordOutput(
+        "RobotState/Control Data/Speaker Radial Velocity", controlData.speakerRadialVelocity());
+  }
+
+  public static Pose2d getRobotPose() {
+    return poseEstimator.getEstimatedPosition();
+  }
+
+  public static Pose2d getOdometryPose() {
+    return odometry.getPoseMeters();
+  }
+
+  public static void resetRobotPose(Pose2d pose) {
+    headingOffset = robotHeading.minus(pose.getRotation());
+    poseEstimator.resetPosition(robotHeading, modulePositions, pose);
+    odometry.resetPosition(robotHeading, modulePositions, pose);
+  }
+
+  public static record ControlData(
+      Rotation2d speakerRobotAngle,
+      double speakerRadialVelocity,
+      Rotation2d speakerArmAngle,
+      boolean hasNoteLocked,
+      boolean hasNoteStaged,
+      boolean isIntaking) {}
+}
---
+++ src/main/java/frc/robot/subsystems/arm/ArmIOTalonFX.java
@@ -0,0 +1,206 @@
+package frc.robot.subsystems.arm;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+
+public class ArmIOTalonFX implements ArmIO {
+  private final TalonFX motor;
+  private final CANcoder cancoder;
+
+  private final StatusSignal<Angle> positionRotations;
+  private final StatusSignal<AngularVelocity> velocityRotationsPerSecond;
+  private final StatusSignal<Voltage> appliedVolts;
+  private final StatusSignal<Current> currentAmps;
+  private final StatusSignal<Temperature> temperatureCelcius;
+  private final StatusSignal<Double> positionSetpointRotations;
+  private final StatusSignal<Double> positionErrorRotations;
+
+  private final StatusSignal<Angle> absolutePosition;
+
+  private final MotionMagicVoltage positionControl;
+  private final VoltageOut voltageControl;
+  private final NeutralOut neutralControl;
+
+  private final TalonFXConfiguration motorConfig;
+  private final CANcoderConfiguration cancoderConfig;
+
+  private Rotation2d positionGoal;
+
+  private boolean hasResetPosition;
+
+  public ArmIOTalonFX() {
+    motor = new TalonFX(ArmConstants.ARM_CAN_ID);
+    cancoder = new CANcoder(ArmConstants.CANCODER_CAN_ID);
+
+    motorConfig = new TalonFXConfiguration();
+    motorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    motorConfig.CurrentLimits.SupplyCurrentLimit = 40.0;
+    motorConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    motorConfig.Feedback.SensorToMechanismRatio = ArmConstants.ARM_GEAR_RATIO;
+    motorConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    motorConfig.MotionMagic.MotionMagicCruiseVelocity =
+        Units.radiansToRotations(ArmConstants.ARM_MAX_VELOCITY.get());
+    motorConfig.MotionMagic.MotionMagicAcceleration =
+        Units.radiansToRotations(ArmConstants.ARM_MAX_ACCELERATION.get());
+    motorConfig.Slot0.kP = ArmConstants.ARM_KP.get();
+    motorConfig.Slot0.kD = ArmConstants.ARM_KD.get();
+    motorConfig.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
+    motorConfig.Slot0.kS = ArmConstants.ARM_KS.get();
+    motorConfig.Slot0.kV = ArmConstants.ARM_KV.get();
+    motorConfig.Slot0.kG = ArmConstants.ARM_KG.get();
+    motor.getConfigurator().apply(motorConfig);
+
+    cancoderConfig = new CANcoderConfiguration();
+    cancoderConfig.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
+    cancoder.getConfigurator().apply(cancoderConfig);
+
+    positionRotations = motor.getPosition();
+    velocityRotationsPerSecond = motor.getVelocity();
+    appliedVolts = motor.getMotorVoltage();
+    currentAmps = motor.getSupplyCurrent();
+    temperatureCelcius = motor.getDeviceTemp();
+    positionSetpointRotations = motor.getClosedLoopReference();
+    positionErrorRotations = motor.getClosedLoopError();
+
+    absolutePosition = cancoder.getAbsolutePosition();
+
+    positionControl = new MotionMagicVoltage(0.0);
+    voltageControl = new VoltageOut(0.0);
+    neutralControl = new NeutralOut();
+
+    positionGoal = new Rotation2d();
+
+    BaseStatusSignal.setUpdateFrequencyForAll(
+        50.0,
+        positionRotations,
+        velocityRotationsPerSecond,
+        appliedVolts,
+        currentAmps,
+        temperatureCelcius,
+        positionSetpointRotations,
+        positionErrorRotations,
+        absolutePosition);
+
+    motor.optimizeBusUtilization(50.0, 1.0);
+    cancoder.optimizeBusUtilization(50.0, 1.0);
+
+    hasResetPosition = false;
+  }
+
+  @Override
+  public void updateInputs(ArmIOInputs inputs) {
+    BaseStatusSignal.refreshAll(
+        positionRotations,
+        velocityRotationsPerSecond,
+        appliedVolts,
+        currentAmps,
+        temperatureCelcius,
+        positionSetpointRotations,
+        positionErrorRotations,
+        absolutePosition);
+    positionSetpointRotations.refresh();
+    positionErrorRotations.refresh();
+
+    inputs.armPosition = Rotation2d.fromRotations(positionRotations.getValueAsDouble());
+    inputs.armVelocityRadPerSec =
+        Units.rotationsToRadians(velocityRotationsPerSecond.getValueAsDouble());
+    inputs.armAppliedVolts = appliedVolts.getValueAsDouble();
+    inputs.armCurrentAmps = currentAmps.getValueAsDouble();
+    inputs.armTemperatureCelsius = temperatureCelcius.getValueAsDouble();
+
+    inputs.armAbsolutePosition =
+        Rotation2d.fromRotations(absolutePosition.getValueAsDouble())
+            .minus(ArmConstants.ARM_ABSOLUTE_ENCODER_OFFSET);
+
+    inputs.positionSetpoint =
+        Rotation2d.fromRotations(positionSetpointRotations.getValueAsDouble());
+    inputs.positionError = Rotation2d.fromRotations(positionErrorRotations.getValueAsDouble());
+
+    inputs.positionGoal = positionGoal;
+  }
+
+  @Override
+  public void stop() {
+    motor.setControl(neutralControl);
+  }
+
+  @Override
+  public void setArmVoltage(double volts) {
+    if (!hasResetPosition) {
+      hasResetPosition =
+          motor
+              .setPosition(
+                  (absolutePosition.getValueAsDouble()
+                          - ArmConstants.ARM_ABSOLUTE_ENCODER_OFFSET.getRotations())
+                      * -1.0)
+              .isOK();
+    }
+    motor.setControl(voltageControl.withOutput(volts).withEnableFOC(false));
+  }
+
+  @Override
+  public void setArmPosition(Rotation2d currentPosition, Rotation2d setpointPosition) {
+    if (!hasResetPosition) {
+      hasResetPosition =
+          motor
+              .setPosition(
+                  (absolutePosition.getValueAsDouble()
+                          - ArmConstants.ARM_ABSOLUTE_ENCODER_OFFSET.getRotations())
+                      * -1.0)
+              .isOK();
+    }
+    positionGoal = setpointPosition;
+    motor.setControl(
+        positionControl
+            .withPosition(setpointPosition.getRotations())
+            .withUpdateFreqHz(1000)
+            .withEnableFOC(false));
+  }
+
+  @Override
+  public void setPID(double kp, double ki, double kd) {
+    motorConfig.Slot0.kP = kp;
+    motorConfig.Slot0.kI = ki;
+    motorConfig.Slot0.kD = kd;
+    motor.getConfigurator().apply(motorConfig);
+  }
+
+  @Override
+  public void setFeedforward(double ks, double kg, double kv) {
+    motorConfig.Slot0.kS = ks;
+    motorConfig.Slot0.kV = kv;
+    motorConfig.Slot0.kG = kg;
+    motor.getConfigurator().apply(motorConfig);
+  }
+
+  @Override
+  public void setProfile(double maxVelocity, double maxAcceleration) {
+    motorConfig.MotionMagic.MotionMagicCruiseVelocity = Units.radiansToRotations(maxVelocity);
+    motorConfig.MotionMagic.MotionMagicAcceleration = Units.radiansToRotations(maxAcceleration);
+    motor.getConfigurator().apply(motorConfig);
+  }
+
+  @Override
+  public boolean atSetpoint() {
+    return Math.abs(positionGoal.getRotations() - positionRotations.getValueAsDouble())
+        <= Units.degreesToRotations(ArmConstants.GOAL_TOLERANCE.get());
+  }
+}
